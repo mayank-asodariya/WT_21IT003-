1794873b8152536e9e7b3bbfc4968ec12870255d
mayank-asodariya <117512100+mayank-asodariya@users.noreply.github.com>
2023-02-16 11:29:49 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,19 @@
+console.log("connected...")
+
+function addItem() {
+
+    const eleInputObj = document.getElementById("idItem");
+    const eleItemLiObj = document.createElement("li");
+    eleItemLiObj.textContent = eleInputObj.value;
+
+   arr =  Array.addItem = eleInputObj;
+
+    document.getElementById("idItemList").appendChild(eleItemLiObj)
+}
+
+function Ass(arr) {
+    eleItemLiObj.sort(arr);
+    document.getElementById("idItemList").appendChild(arr)
+    console.log(arr);
+
+}
